e8d3a0cefa0d90b0c8ac9fbdc3f4638ca96a34d1
Cameron <ralefire@gmail.com>
2014-11-13 18:40:56 -0700

    Added PDF builder functions.

--- Castle/src/Castle/PDF.java
+++ Castle/src/Castle/PDF.java
@@ -1,13 +1,29 @@
+/*
+ * Helpful documentation: http://www.printmyfolders.com/Home/PDFBox-Tutorial
+ */
+
 package Castle;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.pdfbox.exceptions.COSVisitorException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.util.PDFTextStripper;
 
 /**
  * This is a representation of our PDF.
- * @author Xandron
+ * @author Winners
  */
 class PDF {
+    
+    PDDocument document;
+    
     /**
      * This is a map of the hashes to the questions.
      */
@@ -30,14 +46,71 @@ class PDF {
      * @param filename 
      */
     public void load(String filename){}
+    
     /**
      * This saves the PDF to the filename specified.
      * @param filename 
      */
     public void save(String filename){}
+    
     /**
      * This exports the PDF in the final format to the location specified.
      * @param filename 
      */
     public void export(String filename){}
+
+
+    public void buildPDF() {
+        //Document luceneDocument = LucenePDFDocument.getDocument();
+        PDDocument doc = null;
+        PDPage page = null;
+
+        doc = new PDDocument();
+        try{
+           doc = new PDDocument();
+           page = new PDPage();
+
+           doc.addPage(page);
+           PDFont font = PDType1Font.HELVETICA_BOLD;
+
+           PDPageContentStream content = new PDPageContentStream(doc, page);
+           content.beginText();
+           content.setFont( font, 12 ); // set font (mandatory)
+           content.moveTextPositionByAmount( 100, 700 ); // set text position (mandatory)
+           content.drawString("This is an awesome PDF"); // enter text
+           content.endText(); 
+           content.close();
+           doc.save("test.pdf"); // save
+           doc.close(); // close
+        } catch (IOException | COSVisitorException io){
+            System.out.println(io);
+        }
+    }
+    
+  public void loadPDFFromFile(String filePath) throws IOException {
+        if (filePath == null || filePath.equals(""))
+            filePath = "C:/Users/Admin/Documents/NetBeansProjects/TestPDFAction/src/resources/6dot1.pdf";
+    
+        File file = new File(filePath);
+        document = PDDocument.load(file); //This will load a document from a file into PDDocument.
+
+//        document.getNumberOfPages(); //Returns the number of pages in the PDF file.
+//        document.isEncrypted(); //Lets you know if the PDF file is encrypted or not.
+//        document.void print(); //Sends the PDF document to a printer.
+//        document.removePage(int pageNumber); //Removes the page referred to by the page number.
+//        document.save(String fileName); //Saves the PDF file under the file name.
+//        document.silentPrint(); //This will send the PDF to the default printer without prompting the user for any printer settings.
+//        document.close(); //This will close the file.
+        
+        
+    }
+    
+    public void extractText() throws IOException {
+        String content = "";
+        
+        PDFTextStripper stripper = new PDFTextStripper();
+        content = stripper.getText(document);
+        System.out.println(content);
+    }
+        
 }
