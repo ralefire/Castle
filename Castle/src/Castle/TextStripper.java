82cd63d289377e22370d2129e0d3f780b7290eeb
Cameron <ralefire@gmail.com>
2014-12-12 22:13:22 -0700

    Fixed package bits

--- Castle/src/Castle/TextStripper.java
+++ Castle/src/Castle/TextStripper.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package testpdfaction;
+package Castle;
 
 import org.apache.pdfbox.util.PDFTextStripper;
 import java.io.IOException;
